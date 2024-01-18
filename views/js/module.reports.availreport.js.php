9e4f364c50179ecc08db70cf6fff0e975c38a8ab
lkaprielian <lisakaprielian.lk@gmail.com>
2024-01-18 19:23:57 +0100

    Update module.reports.availreport.js.php

--- views/js/module.reports.availreport.js.php
+++ views/js/module.reports.availreport.js.php
@@ -18,7 +18,7 @@
 				this.refresh_counters = this.createCountersRefresh(1);
 				this.filter = new CTabFilter($('#reports_availreport_filter')[0], filter_options);
 				var filter_item = this.filter._active_item;
-				this.filter.on(TABFILTERITEM_EVENT_EXPAND => {
+				this.filter.on(TABFILTER_EVENT_URLSET, (ev) => {
 					// const url = new URL(this.refresh_url, 'http://example.com');
 					// for(var key of url.searchParams.keys()) {
 					// 	if (key == 'from' || key == 'to') {
@@ -37,11 +37,11 @@
 					// this.refresh_url=url.pathname.slice(1) + '?' + url.searchParams.toString();
 					// this.refresh();
 					// }
-					// let url = new Curl('', false);
-					// url.setArgument('action', 'availreport.view.refresh');
-					// this.refresh_url = url.getUrl();
-					// this.unscheduleRefresh();
-					// this.refresh();
+					let url = new Curl('', false);
+					url.setArgument('action', 'availreport.view.refresh');
+					this.refresh_url = url.getUrl();
+					this.unscheduleRefresh();
+					this.refresh();
 					
 					if (this.filter._active_item.hasCounter()) {
 						$.post('zabbix.php', {
