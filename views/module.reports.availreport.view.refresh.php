f6cbe822556b9a16434bc854f202909f5fa16158
lkaprielian <lisakaprielian.lk@gmail.com>
2024-02-13 19:58:11 +0100

    update

--- views/module.reports.availreport.view.refresh.php
+++ views/module.reports.availreport.view.refresh.php
@@ -5,12 +5,21 @@ $output = [
 ];
 
 
-if ($data['warning']) {
-	error($data['warning']);
-}
+// if ($data['warning']) {
+// 	error($data['warning']);
+// }
+
+// if (($messages = getMessages()) !== null) {
+// 	$output['messages'] = $messages->toString();
+// }
 
 if (($messages = getMessages()) !== null) {
 	$output['messages'] = $messages->toString();
+}
+
+if (CWebUser::$data['debug_mode'] == GROUP_DEBUG_MODE_ENABLED) {
+	CProfiler::getInstance()->stop();
+	$output['debug'] = CProfiler::getInstance()->make()->toString();
 }
 
 echo json_encode($output);
