2bce094aaff8704aaf0fafdcd151b5645cf2c63c
lkaprielian <lisakaprielian.lk@gmail.com>
2024-02-14 01:41:30 +0100

    edit

--- partials/reports.availreport.filter.php
+++ partials/reports.availreport.filter.php
@@ -171,17 +171,7 @@ if (array_key_exists('render_html', $data)) {
 	->setAttribute('data-template', 'reports.availreport.filter') ///here
 	->addItem($template)
 	->show();
-	$html_page->setControls((new CForm('get'))
-		->setAttribute('aria-label', _('Main filter'))
-		->addItem((new CList())
-			->addItem([
-				new CLabel(_('Mode'), $select_mode->getFocusableElementId()),
-				(new CDiv())->addClass(ZBX_STYLE_FORM_INPUT_MARGIN),
-				$select_mode
-			])
-		)
-		->setName('report2')
-	);
+
 ?>
 <script type="text/javascript">
 	let template = document.querySelector('[data-template="reports.availreport.filter"]');
