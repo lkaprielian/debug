061d73166069b8ec7ddc4c856e6941f186ef50fa
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-01 12:37:30 +0100

    Update reports.availreport.filter.php

--- partials/reports.availreport.filter.php
+++ partials/reports.availreport.filter.php
@@ -144,11 +144,11 @@ $template = (new CForm('get'))
 	->addItem([
 		$template,
 		(new CSubmitButton(null))->addClass(ZBX_STYLE_FORM_SUBMIT_HIDDEN)
-		// (new CVar('filter_name', '#{filter_name}'))->removeId(),
-		// (new CVar('filter_show_counter', '#{filter_show_counter}'))->removeId(),
-		// (new CVar('filter_custom_time', '#{filter_custom_time}'))->removeId(),
-		// (new CVar('from', '#{from}'))->removeId(),
-		// (new CVar('to', '#{to}'))->removeId()
+		(new CVar('filter_name', '#{filter_name}'))->removeId(),
+		(new CVar('filter_show_counter', '#{filter_show_counter}'))->removeId(),
+		(new CVar('filter_custom_time', '#{filter_custom_time}'))->removeId(),
+		(new CVar('from', '#{from}'))->removeId(),
+		(new CVar('to', '#{to}'))->removeId()
 		// (new CVar('hostids', '#{hostids}'))->removeId()
 
 		// (new CVar('sort', '#{sort}'))->removeId(),
