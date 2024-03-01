7278879bf714b05d283abdd7622b358f7fc0881d
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-01 12:10:19 +0100

    Update reports.availreport.filter.php

--- partials/reports.availreport.filter.php
+++ partials/reports.availreport.filter.php
@@ -157,7 +157,7 @@ $template = (new CForm('get'))
 		(new CVar('filter_custom_time', '#{filter_custom_time}'))->removeId(),
 		(new CVar('sort', '#{sort}'))->removeId(),
 		(new CVar('sortorder', '#{sortorder}'))->removeId()
-		
+
 		// (new CVar('hostids', '#{hostids}'))->removeId()
 
 		// (new CVar('sort', '#{sort}'))->removeId(),
@@ -182,7 +182,7 @@ if (array_key_exists('render_html', $data)) {
 	->show();
 ?>
 <script type="text/javascript">
-	let template = document.querySelector('[data-template="reports.availreport.filter"]');
+	let template = document.querySelector('[data-template="monitoring.problem.filter]');
 
 	function render(data, container) {
 		// "Save as" can contain only home tab, also home tab cannot contain "Update" button.
