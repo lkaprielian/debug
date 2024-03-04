284e75eb6665573856189134ae8aade1deefed3c
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-04 10:56:30 +0100

    Update module.reports.availreport.view.refresh.php

--- views/module.reports.availreport.view.refresh.php
+++ views/module.reports.availreport.view.refresh.php
@@ -1,17 +1,28 @@
 <?php
 
+// $output = [
+// 	'body' => (new CPartial('reports.availreport.view.html', $data))->getOutput()
+// ];
+
+// if ($data['warning']) {
+// 	error($data['warning']);
+// }
+
+// if (($messages = getMessages()) !== null) {
+// 	$output['messages'] = $messages->toString();
+// }
+
+// echo json_encode($output);
+
 $output = [
-	'body' => (new CPartial('reports.availreport.view.html', $data))->getOutput()
+	'body' => (new CPartial('reports.availreport.view.html', $data))->getOutput(),
+	'groupids' => $data['filter']['groupids']
 ];
-
-
-if ($data['warning']) {
-	error($data['warning']);
-}
 
 if (($messages = getMessages()) !== null) {
 	$output['messages'] = $messages->toString();
 }
 
 echo json_encode($output);
+
 ?>
