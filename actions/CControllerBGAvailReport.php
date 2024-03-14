aefe41ab94ad2d89443357f25caa3053311f653e
lkaprielian <lisakaprielian.lk@gmail.com>
2024-03-14 16:52:29 +0100

    update

--- actions/CControllerBGAvailReport.php
+++ actions/CControllerBGAvailReport.php
@@ -14,7 +14,7 @@ use CWebUser;
 abstract class CControllerBGAvailReport extends CController {
 
 	// Filter idx prefix.
-	// const FILTER_IDX = 'web.avail_report.filter';
+	const FILTER_IDX = 'web.avail_report.filter';
 	// const FILTER_IDX = 'web.monitoring.host';
 	// const FILTER_IDX = 'web.monitoring.problem';
 
