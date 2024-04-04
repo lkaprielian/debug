efac410750e21fa2c0332d7b1cd44145ea2f3bcd
lkaprielian <lisakaprielian.lk@gmail.com>
2024-04-04 16:43:32 +0200

    Update CControllerBGAvailReport.php

--- actions/CControllerBGAvailReport.php
+++ actions/CControllerBGAvailReport.php
@@ -14,9 +14,11 @@ use CWebUser;
 abstract class CControllerBGAvailReport extends CController {
 
 	// Filter idx prefix.
-	const FILTER_IDX = 'web.avail_report.filter';
+	// const FILTER_IDX = 'web.avail_report.filter';
+	const FILTER_IDX = 'web.toptriggers.filter'
 	// const FILTER_IDX = 'web.monitoring.host';
 	// const FILTER_IDX = 'web.monitoring.problem';
+
 
 
 	// Filter fields default values.
