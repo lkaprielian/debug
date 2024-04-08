5574e8a0a202b53bca14e2a69ae913de02aa1783
lkaprielian <lisakaprielian.lk@gmail.com>
2024-04-08 11:15:02 +0200

    update

--- views/module.reports.availreport.view copy.php
+++ views/module.reports.availreport.view copy.php
@@ -25,8 +25,8 @@ if ($data['action'] == 'availreport.view') {
 
 	if ($web_layout_mode == ZBX_LAYOUT_NORMAL) {
 		$filter = (new CTabFilter())
-			// ->setId('reports_availreport_filter')
-			->setId('monitoring_problem_filter')
+			->setId('reports_availreport_filter')
+			// ->setId('monitoring_problem_filter')
 			->setOptions($data['tabfilter_options'])
 			->addTemplate(new CPartial($data['filter_view'], $data['filter_defaults']));
 
